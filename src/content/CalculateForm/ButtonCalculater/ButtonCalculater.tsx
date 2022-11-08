c076f23e4c9bcf9e3c0a97a543c55c50acb19e7f
BrynzevDmitrii <Elf_happy@mail.ru>
2022-11-08 15:47:16 +0300

    style text & btnSerge

---
+++ src/content/CalculateForm/ButtonCalculater/ButtonCalculater.tsx
@@ -0,0 +1,5 @@
+export const ButtonCalculater=({ children}:{children:string}): JSX.Element=>{
+    return(
+        <button>{children}</button>
+    )
+}
--- src/content/CalculateForm/CalculateForm.tsx
+++ src/content/CalculateForm/CalculateForm.tsx
@@ -1,12 +1,21 @@
-import styles from './CalculateForm.module.scss'
+
+import { ButtonCalculater } from "./ButtonCalculater/ButtonCalculater";
+import styles from "./CalculateForm.module.scss";
 export const CalculateForm = () => {
-    return(
-        <>
-            <p className={styles["calculate_options"]}>Выберите пораметры</p>
-            <p>Нажмите кнопку {'<<Расчитать>>'}</p>
-            <button>Расчитать</button>
-            <a href="#">Найти Отделение</a>
-        <p>Предварительный расчет. Не является публичной офертой.</p>
-        </>
-    )
-}
+  return (
+    <div className={styles["calculate"]}>
+      <div>
+        <p className={styles["calculate_options"]}>Выберите параметры</p>
+        <p className={styles["calculate_title"]}>
+          Нажмите кнопку {"<<Расчитать>>"}
+        </p>
+      </div>
+      <div className={styles["calculate_btn-wrapper"]}>
+        <ButtonCalculater>Расчитать</ButtonCalculater>
+        <a  className={styles["calculate_btn-serge"]} href="#">
+            <p className={styles["calculate_btn-text"]}> Найти Отделение </p> </a>
+      </div>
+      <p className={styles["calculate_afert"]}>Предварительный расчет. Не является публичной офертой.</p>
+    </div>
+  );
+};
