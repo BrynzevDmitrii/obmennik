6bc9ca2797e88009e8b91366b09f6ff939e13a91
BrynzevDmitrii <Elf_happy@mail.ru>
2022-11-08 16:57:52 +0300

    finidh style for calculate form

--- src/content/CalculateForm/ButtonCalculater/ButtonCalculater.tsx
+++ src/content/CalculateForm/ButtonCalculater/ButtonCalculater.tsx
@@ -1,5 +1,7 @@
-export const ButtonCalculater=({ children}:{children:string}): JSX.Element=>{
+import styles from "./ButtonCalculater.module.scss"
+
+export const ButtonCalculater = ({ children}:{children:string}): JSX.Element=>{
     return(
-        <button>{children}</button>
+        <button className={styles["calculate_btn"]}><p className={styles["calculate_btn-text"]}>{children}</p></button>
     )
 }
