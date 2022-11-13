b9615f57ddbb5f10966a2b53854b1d37988fe999
BrynzevDmitrii <Elf_happy@mail.ru>
2022-11-13 21:53:40 +0300

    open custom selection

--- src/shared/Selects/BaseSelect/BaseSelect.tsx
+++ src/shared/Selects/BaseSelect/BaseSelect.tsx
@@ -1,45 +1,45 @@
-import * as React from 'react';
-import {  useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import classNames from 'classnames';
+import { useEffect, useRef, useState } from 'react';
+import Item from '../../../widgets/Item/Item';
 import styles from './BaseSelect.module.scss'
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
 
 export default function BaseSelect() {
-  const theme = useTheme();
-  const [personName, setPersonName] = React.useState<string[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectionValue, setSelectionValue] = useState('В офисе (наличные)');
 
- const isOpen =()=>{
-  console.log('open select');
+ const openSelect =()=>{
+  setIsOpen(!isOpen);
+ }
+
+ useEffect(()=>{
+  const y  = document.getElementsByTagName('ariaSelected');
+  console.log(y);
+  
+ },[])
+
+const vref = useRef(null)
+ const selectValue = () =>{
+console.log('jhuo')
  }
 
   return (
     <div className={styles['select']}>
         <div className={styles['select_value']}>
-          <button className={styles['select_btn']} onClick = {()=>isOpen()}>
+          <button className={styles['select_btn']} onClick = {()=>openSelect()}>
             <span className={styles['select_span']}>
-              В офисе (наличные)
+              {selectionValue}
               </span></button>
         </div>
-          <ul className={styles['select_list']}>
-            <li aria-selected = 'true' className={styles['select_item']}>В офисе (наличные)</li>
-            <li aria-selected = 'false'className={styles['select_item']}>В офисе (безналично)</li>
-            <li aria-selected = 'false' className={styles['select_item']}>В интернет-банке и мобильном банке</li>
-            <li aria-selected = 'false' className={styles['select_item']}>В спецкассе по обмену валюты</li>
-            <li aria-selected = 'false' className={styles['select_item']}>Покупка по карте</li>
+          <ul  ref ={vref} className= {classNames(isOpen? styles['select_list_open'] : styles['select_list_closed'])}>
+            <li id='item-selected'  aria-selected = 'true' className={styles['select_item']}>В офисе (наличные)</li>
+            <li key={2}  aria-selected = 'false'className={styles['select_item']}>В офисе (безналично)</li>
+            <li key={3}  aria-selected = 'false' className={styles['select_item']}>В интернет-банке и мобильном банке</li>
+            <li key={4}  aria-selected = 'false' className={styles['select_item']}>В спецкассе по обмену валюты</li>
+            <li key={5}  aria-selected = 'false' className={styles['select_item']}>Покупка по карте</li>
           </ul>
     </div>
   );
 }
+
+
