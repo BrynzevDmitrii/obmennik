ca3dd0f76dc8beeaace6052b4bf1c2ef836c0313
BrynzevDmitrii <Elf_happy@mail.ru>
2022-11-25 16:13:49 +0300

    replsce valutCurrent & css

--- src/shared/BaseInput/BaseInput.tsx
+++ src/shared/BaseInput/BaseInput.tsx
@@ -1,43 +1,47 @@
-import { FocusEvent, FormEvent, KeyboardEvent, useEffect, useState } from "react";
+import { FocusEvent, FormEvent, KeyboardEvent, useEffect, useRef, useState } from "react";
 import styles from "./BaseInput.module.scss";
 
 const defaultValue = "0";
 
+
 export const BaseInput = ({ valut }: { valut: string }): JSX.Element => {
 
   const [actualValute, setActualValute] = useState(valut)
+  const valueRef = useRef<HTMLInputElement>(null);
 
   useEffect(()=>{setActualValute(valut)},[valut])
+
   useEffect(()=>{replaseSimbol(actualValute)},[actualValute])
- 
-  const addSimbolMany = (e?: FocusEvent<HTMLInputElement, Element>, av:string) => {
-  const value = e.target.value
-    e.target.value  = value  + ' '+ av
+
+
+  const replaseSimbol = (activValut: string)=>{
+   let tr = valueRef.current.value;
+   let rtu = tr.split(' ')
+    rtu[1] = activValut;
+    let e = rtu.join(' ')
+    valueRef.current.value = e
   }
 
-  const replaseSimbol = (newSimbol)=>{
-    addSimbolMany(newSimbol)
+  const addSimbolMany = (e: FocusEvent<HTMLInputElement, Element>) => {
+    const value = e.target.value
+    e.target.value  = value  + ' ' + actualValute
+    
   }
   
-  const spaceAfterThirdCharacter=(e: FormEvent<HTMLInputElement>, y:string)=>{
-    const userCurrent = e.target.value.replace(/\D/g, '')
-    e.target.value = new Intl.NumberFormat( 'ru-RU', { style: 'decimal', currency: 'RUB' }).format(userCurrent)
+  const spaceAfterThirdCharacter=(e: FormEvent<HTMLInputElement>)=>{
+     const userCurrent = e.target.value.replace(/\D/g, '')
+    e.target.value = new Intl.NumberFormat( 'ru-RU', { style: 'decimal'}).format(userCurrent)
   }
-
-  const prohibitionEnteringLetters=(e: KeyboardEvent<HTMLInputElement>, )=>{
-    // e.target.value = e.target.value.replace( /[a-z]/g, '');
-  }
-
   
 
   return (
-    <div className={styles["input_wrapper"]} >
+    <div  className={styles["input_wrapper"]} >
         <input 
-        className={styles["input"]} 
-        placeholder={ defaultValue + actualValute}
-        onBlur={(e)=>{addSimbolMany(e, actualValute)}}
-        onInput = {(e)=>{spaceAfterThirdCharacter(e,actualValute)}} 
-        onKeyUp = {(e)=>{prohibitionEnteringLetters(e)}}
+        ref = {valueRef }
+        className = {styles["input"]} 
+        placeholder = { defaultValue + actualValute}
+        onBlur = {(e)=>{addSimbolMany(e)}}
+        onInput = {(e)=>{spaceAfterThirdCharacter(e)}}   
         
                   />
         <label className={styles["input_label"]} >Сумма обмена</label>
