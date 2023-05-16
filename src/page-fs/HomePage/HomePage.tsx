4329c323be3f638f12403351bbd490167b9c3e50
BrynzevDmitrii <elf_happy@mail.ru>
2023-05-16 12:37:03 +0300

    triger btn

--- src/page-fs/HomePage/HomePage.tsx
+++ src/page-fs/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import Layout from "../../widgets/layouts/Loyaut";
 import { CurrencyConverter } from "../../widgets/CurrencyConverter/CurrencyConverter";
 import { Previu } from "../../shared/Previu/Previu";
+import { ListCurrencyToday } from "../../widgets/ListValueToday/ListValueToday";
 
 
 export const HomePage = (): JSX.Element => {
   return (
     <Layout>
       <Previu />
+      <ListCurrencyToday />
       <CurrencyConverter />
       
     </Layout>
