0253807e3998ac911a85d8c3cef27edda95629b0
BrynzevDmitrii <elf_happy@mail.ru>
2023-05-16 10:34:42 +0300

    Calculater form

--- src/features/CalculateForm/CalculateForm.tsx
+++ src/features/CalculateForm/CalculateForm.tsx
@@ -1,38 +1,41 @@
-
-import useEnhancedEffect from "@mui/material/utils/useEnhancedEffect";
 import { useEffect, useState } from "react";
 import { ButtonCalculater } from "../../shared/ButtonCalculater/ButtonCalculater";
 import styles from "./CalculateForm.module.scss";
 import { ResultCalculateForm } from "./ResultCalculateForm/ResultCalculateForm";
-export const CalculateForm = () => {
-  const [resultSection, setResultSection] = useState(false)
-  useEffect(()=>{
-    setResultSection(false)
-  }, [])
+export const CalculateForm = ({availableValute, getValute}:{ availableValute: string, getValute: string }) => {
+  const [resultSection, setResultSection] = useState(false);
+  useEffect(() => {
+    setResultSection(false);
+  }, []);
   const open = () => {
-    setResultSection(true)
-  }
+    setResultSection(true);
+  };
   return (
-    
     <div className={styles["calculate"]}>
-      {(resultSection == false )? 
-      <><div>
+      {resultSection == false ? (
+        <>
+          <div>
             <p className={styles["calculate_options"]}>Выберите параметры</p>
             <p className={styles["calculate_title"]}>
               Нажмите кнопку {"<<Расчитать>>"}
             </p>
-          </div><div className={styles["calculate_btn-wrapper"]}>
-              <ButtonCalculater open={open}>Расчитать</ButtonCalculater>
-            </div></> :
-      <ResultCalculateForm />}
-     
-       
-        <div className={styles["calculate_btn-wrapper"]}>
-        <a  className={styles["calculate_btn-serge"]} href="#">
-            <p className={styles["calculate_btn-text"]}> Найти Отделение </p> </a>
+          </div>
+          <div className={styles["calculate_btn-wrapper"]}>
+            <ButtonCalculater open={open}>Расчитать</ButtonCalculater>
+          </div>
+        </>
+      ) : (
+        <ResultCalculateForm availableValute={availableValute} getValute = {getValute} />
+      )}
+
+      <div className={styles["calculate_btn-wrapper"]}>
+        <a className={styles["calculate_btn-serge"]} href="#">
+          <p className={styles["calculate_btn-text"]}> Найти Отделение </p>{" "}
+        </a>
       </div>
-      <p className={styles["calculate_afert"]}>Предварительный расчет. Не является публичной офертой.</p>
+      <p className={styles["calculate_afert"]}>
+        Предварительный расчет. Не является публичной офертой.
+      </p>
     </div>
-  
   );
 };
--- src/shared/ButtonCalculater/ButtonCalculater.tsx
+++ src/shared/ButtonCalculater/ButtonCalculater.tsx
@@ -2,6 +2,12 @@ import styles from "./ButtonCalculater.module.scss"
 
 export const ButtonCalculater = ({ children, open}:{children:string, open: ()=>void}): JSX.Element=>{
     return(
-        <button onClick={()=>open()} className={styles["calculate_btn"]}><p className={styles["calculate_btn-text"]}>{children}</p></button>
+        <button 
+        onClick={()=>open()}
+        className={styles["calculate_btn"]}>
+                <p className={styles["calculate_btn-text"]}>
+                {children}
+                </p>
+        </button>
     )
 }
