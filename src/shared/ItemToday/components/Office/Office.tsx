6dd3d7f3f9b2f7299ef1bb50a02939f785bc8bf9
BrynzevDmitrii <elf_happy@mail.ru>
2023-07-22 10:07:17 +0300

    office cash

--- src/shared/ItemToday/components/Office/Office.tsx
+++ src/shared/ItemToday/components/Office/Office.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import { Root } from "../../../ListToday/type/currentType";
-import { PrevValute } from "../HOC/PrevValute";
+import { PrevValute } from "../PrevValute/PrevValute";
 
 interface OfficeProps {
     data: Root
@@ -11,6 +11,8 @@ export const Office: FunctionComponent<OfficeProps> = (props: OfficeProps) => {
         <li>
             <PrevValute valuteCurrents={Object.values(props.data.Valute).filter(item => item.CharCode === 'USD')} current = {'до 30 000.00 '} />
             <PrevValute valuteCurrents={Object.values(props.data.Valute).filter(item => item.CharCode === 'USD')} current = {'от 30 000.00'} />
+            <PrevValute valuteCurrents={Object.values(props.data.Valute).filter(item => item.CharCode === 'EUR')} current = {'до 30 000.00 '} />
+            <PrevValute valuteCurrents={Object.values(props.data.Valute).filter(item => item.CharCode === 'EUR')} current = {'от 30 000.00'} />
         </li>
     </> );
 }
